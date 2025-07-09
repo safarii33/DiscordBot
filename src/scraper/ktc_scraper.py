7b557ab7ba7ffdb12741c9da797b6709d09bb7c8
Safarii Hunt <harlemhuskie33@gmail.com>
2025-07-08 21:54:28 -0500

    New features and restructuring

--- src/scraper/ktc_scraper.py
+++ src/scraper/ktc_scraper.py
@@ -5,7 +5,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
-from scraper.web_driver import WebDriverManager  # ✅ Import the class
+from src.scraper.web_driver import WebDriverManager  # ✅ Import the class
 
 import time
 
